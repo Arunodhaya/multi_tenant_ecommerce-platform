7d789b982b76a315b92670dfc8de0a26c9e7c571
aruna <aruna@gogosoon.com>
2024-01-20 16:33:57 +0530

    [Aruna] adds migration to create roles

---
+++ migrations/20240120103822-Insert-3-main-roles-in-user-table.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('roles', [
+      { role_name: 'CUSTOMER', createdAt: new Date(), updatedAt: new Date() },
+      { role_name: 'ADMIN', createdAt: new Date(), updatedAt: new Date() },
+      { role_name: 'STORE_OWNER', createdAt: new Date(), updatedAt: new Date() },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('roles', {
+      role_name: ['CUSTOMER', 'ADMIN', 'STORE_OWNER'],
+    }, {});
+  }
+};
