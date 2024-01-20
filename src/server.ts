8eab22dc5f5e27ac0a201cc3b4035fc2537d8495
aruna <aruna@gogosoon.com>
2024-01-20 12:54:28 +0530

    [Aruna] adds app port in .env

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import * as dotenv from 'dotenv'
+
+dotenv.config({ path: __dirname + "/../.env.local" });
+dotenv.config({ path: __dirname + "/../.env" });
 
 const app= express();
-const port = 8000;
+const port = process.env.APP_PORT;
 
 //Middleware to parse JSON requests
 app.use(bodyParser.json())
