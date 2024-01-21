ae69c2a76c0e18bea81c07b8ade82e2d7bdfa337
aruna <aruna@gogosoon.com>
2024-01-21 17:50:39 +0530

    [Aruna] Adds function to generate customer token

--- src/helpers/authHelper.ts
+++ src/helpers/authHelper.ts
@@ -1,6 +1,7 @@
 import { UserModel } from "../model/UserModel";
 import jwt from "jsonwebtoken";
 import * as dotenv from 'dotenv'
+import { CustomerModel } from "../model/CustomerModel";
 
 
 dotenv.config({ path: __dirname + "/../.env.local" });
@@ -9,10 +10,18 @@ dotenv.config({ path: __dirname + "/../.env" });
 // Secret key for JWT
 export const secretKey = process.env.JWT_TOKEN;
 
-export const generateToken = (user:UserModel)=>{
+export const generateAdminToken = (user:UserModel)=>{
     return jwt.sign(
         { user_id: user.user_id, email: user.email },
         secretKey,
-        { expiresIn: "1h" }
+        { expiresIn: "1d" }
       );
+}
+
+export const generateCustomerToken = (customer:CustomerModel)=>{
+  return jwt.sign(
+      { customer_id: customer.customer_id, email: customer.email },
+      secretKey,
+      { expiresIn: "1d" }
+    );
 }
